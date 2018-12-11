8e32c443eac848918a522a9cd27fdacc501239cf
Filippo Foladore <filippo.foladore@gmail.com>
2018-12-11 01:39:57 +0100

    Tabella studenti

--- public/js/registroCode.js
+++ public/js/registroCode.js
@@ -1,24 +1,13 @@
-$(document).ready(function()
-{
-    
+$(document).ready(function () {
+    if (screen.width < 420) {
+        $('ul li a').css('margin-right', '40px')
+        //$('#nav:checked~.nav-wrapper ul li a').css('margin-right', '40px')
+    } else if (screen.width > 420) {
+        $('ul li a').css('margin-right', '0px')
+    }
 
-    console.log(screen.width)
-    console.log(screen.height)
-    $(window).resize(function(){
-        if (screen.width < 420){
-            console.log("<420")
-            $('ul li a').css('margin-right', '40px')
-            //$('#nav:checked~.nav-wrapper ul li a').css('margin-right', '40px')
-        } else if (screen.width >420){
-            console.log(">420")
-            $('ul li a').css('margin-right', '0px')
-        }
-    })
-   
-   
-$('#classList li').on('click', function()
-{
-        var id = ""; 
+    $('#classList2 li').on('click', function () {
+        var id = "";
         id = $(this).attr('id');
         $.ajax({
             url: 'manage/classes/' + id,
@@ -27,6 +16,7 @@ $('#classList li').on('click', function()
             dataType: 'json',
             success: function (data) {
                 //pulizia dei div prima di aggiungere i dati
+                console.log(data)
                 $("#className").empty();
                 $("#students").empty();
                 $('#invisibleId').empty();
@@ -35,32 +25,40 @@ $('#classList li').on('click', function()
                 var classId = data['classes'][0]['_id'];
                 var className = data['classes'][0]['cName'];
                 var studentiOrdinato = data['classes'][0]['student'];
-                studentiOrdinato.sort(function(a, b){
-                    var nameA=a.lname.toLowerCase(), nameB=b.lname.toLowerCase()
-                    if (nameA < nameB) return -1 
+                studentiOrdinato.sort(function (a, b) {
+                    var nameA = a.lname.toLowerCase(),
+                        nameB = b.lname.toLowerCase()
+                    if (nameA < nameB) return -1
                     if (nameA > nameB) return 1
-                    return 0 
+                    return 0
                 })
-                $('#invisibleId').append("<p>"+classId+"</p>");
+                $('#invisibleId').append("<p>" + classId + "</p>");
                 $("#className").append("<p>" + className + '</p>');
 
-                $('#studentsTable').append("<table id='studTable'> <tr> <td>Nome</td> <td>Cognome</td> <td>Voti</td> </tr>")
+
+                $('#studentsTable').append("<table id='table'> <thead> <tr> <td>Nome</td> <td>Cognome</td> <td>Voti</td> </thead> <tbody>")
                 for (var i = 0; i < data['classes'][0]['student'].length; i++) {
-                    $('#studTable').append("<tr>"+
-                    "<td>" + data['classes'][0]['student'][i]['name'] + "</td>"+
-                    "<td>" + data['classes'][0]['student'][i]['lname']+ "</td>"+
-                    "<td>" + data['classes'][0]['student'][i]['_id']+ "</td>"+
-                    "</tr>"
+                    $('#table').append("<tr>" +
+                        "<td>" + data['classes'][0]['student'][i]['name'] + "</td>" +
+                        "<td>" + data['classes'][0]['student'][i]['lname'] + "</td>" +
+                        "<td>" + "<a href='#' class='buttonStud' id='" +
+                        data['classes'][0]['student'][i]['_id'] + "'><i class='fas fa-arrow-circle-right'></i></a>" + "</td>" +
+                        "</tr>"
                     )
                 }
-                $('#studTable').append("</table>")
-        }
-    });
+                $('#studentsTable').append('</tbody> </table>')
+                
+            }
+        });
+    })
+
 })
+
+$(window).resize(function () {
+    if (screen.width < 420) {
+        $('ul li a').css('margin-right', '40px')
+        //$('#nav:checked~.nav-wrapper ul li a').css('margin-right', '40px')
+    } else if (screen.width > 420) {
+        $('ul li a').css('margin-right', '0px')
+    }
 })
-
-
-
-
-
-

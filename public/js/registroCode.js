f2ca91b0095b39a980b55cd667fcabdacab31e23
Filippo Foladore <filippo.foladore@gmail.com>
2018-12-12 01:46:54 +0100

    Button registro v1

--- public/js/registroCode.js
+++ public/js/registroCode.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
     } else if (screen.width > 420) {
         $('ul li a').css('margin-right', '0px')
     }
-
+    var obj = []
+    var arr = new Array ()
     $('#classList2 li').on('click', function () {
         var id = "";
         id = $(this).attr('id');
@@ -16,12 +17,12 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 //pulizia dei div prima di aggiungere i dati
-                console.log(data)
                 $("#className").empty();
                 $("#students").empty();
                 $('#invisibleId').empty();
                 $('#studentsTable').empty()
-                $('#addStudent').show();
+                //$('#infobox').show();
+                
                 var classId = data['classes'][0]['_id'];
                 var className = data['classes'][0]['cName'];
                 var studentiOrdinato = data['classes'][0]['student'];
@@ -37,7 +38,25 @@ $(document).ready(function () {
 
 
                 $('#studentsTable').append("<table id='table'> <thead> <tr> <td>Nome</td> <td>Cognome</td> <td>Voti</td> </thead> <tbody>")
+                // for (var i = 0; i < data['classes'][0]['student'].length; i++) {
+                //     $('#table').append("<tr>" +
+                //         "<td>" + data['classes'][0]['student'][i]['name'] + "</td>" +
+                //         "<td>" + data['classes'][0]['student'][i]['lname'] + "</td>" +
+                //         "<td>" + "<a href='#' class='buttonStud' id='" +
+                //         data['classes'][0]['student'][i]['_id'] + "'><i class='fas fa-arrow-circle-right'></i></a>" + "</td>" +
+                //         "</tr>"
+                //     )
+                // }
+                
                 for (var i = 0; i < data['classes'][0]['student'].length; i++) {
+                    obj.push({
+                        _id: data['classes'][0]['student'][i]['_id'],
+                        name: data['classes'][0]['student'][i]['name'],
+                        lname: data['classes'][0]['student'][i]['lname'],
+                        gender: data['classes'][0]['student'][i]['gender'],
+                        voti: data['classes'][0]['student'][i]['grades']
+                    })
+                    
                     $('#table').append("<tr>" +
                         "<td>" + data['classes'][0]['student'][i]['name'] + "</td>" +
                         "<td>" + data['classes'][0]['student'][i]['lname'] + "</td>" +
@@ -45,12 +64,54 @@ $(document).ready(function () {
                         data['classes'][0]['student'][i]['_id'] + "'><i class='fas fa-arrow-circle-right'></i></a>" + "</td>" +
                         "</tr>"
                     )
+                    
                 }
+
+                console.log(JSON.stringify(obj))
+                localStorage.setItem('students', JSON.stringify(obj))
                 $('#studentsTable').append('</tbody> </table>')
-                
             }
         });
     })
+
+    $('#studentsTable').on('click', 'a', function(){
+        var studID = "";
+        var classID = $('#invisibleId').text()
+        var dataSent = {
+            classid: $('#invisibleId').text()
+        }
+        studID = $(this).attr('id');
+        // $('.buttonStud').prevUntil('a').css('background-color', 'red')
+        var currRow = $(this).closest("tr")
+        var c1val = currRow.find('td:eq(0)').text()
+        var c2val = currRow.find('td:eq(1)').text()
+        var stud = JSON.stringify(localStorage.getItem('students'))
+        console.log(stud.name)
+       
+        // console.log("stud: "+studID)
+        // console.log(dataSent)
+        // console.log(JSON.stringify(dataSent))
+        // $.ajax ({
+        //     url: '/registro/getStudents/' + studID,
+        //     contentType: 'application/json',
+        //     type: 'POST',
+        //     data: JSON.stringify(dataSent),
+        //     success: function(data){
+        //         console.log('success')
+        //         console.log(data)
+        //        var d = JSON.stringify(data);
+        //        $('#infobox').show()
+        //        //console.log(d)
+
+        //        //var filter = d.filter(element => element._id == studID)
+        //        //console.log(filter)
+        //     }
+        // })
+
+    })
+   
+
+
 
 })
 
@@ -62,3 +123,4 @@ $(window).resize(function () {
         $('ul li a').css('margin-right', '0px')
     }
 })
+
--- routes/registro.js
+++ routes/registro.js
@@ -7,4 +7,27 @@ router.get('/', function(req,res){
     res.render('registro')
 })
 
+router.post('/getStudents/:id', function(req,res){
+
+    // Teacher.findById(
+    //     {_id: req.user.id, "classes._id": req.body.classid, "classes.student._id": req.params.id},
+        
+    //     (err, result) => {
+    //         if (err) {console.log("err"); console.log(err)}
+    //         else {
+    //             console.log(result)
+    //             res.json(result)
+    //         }
+    //     }
+    // )
+    Teacher.findById(
+        {"classes._id": req.body.classid},
+        'classes',
+     (err, result) => {
+        if (err) {console.log(err)}
+        else {console.log(result); res.json(result)}
+    }
+    )
+})
+
 module.exports = router;

f4cd0b595d3e997720beee1f8a05afe93d70570e
Filippo Foladore <filippo.foladore@gmail.com>
2018-12-21 01:20:44 +0100

    Random v0.6

--- public/js/randomCode.js
+++ public/js/randomCode.js
@@ -1,31 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#classList2 li').on('click', function(){
+    $('#classList2 li').on('click', function () {
         let stud = []
         localStorage.clear()
         localStorage.setItem('rID', $(this).attr('id'));
-        console.log(localStorage.getItem('rID'))
-       $.ajax({
-        url: 'random/getClass/' + localStorage.getItem('rID'),
-        contentType: 'application/json',
-        type: 'POST',
-        dataType: 'json',
-        success: function(data){
-            console.log(data)
-            for (let i = 0; i<data['classes'][0]['student'].length; i++){
-                stud.push({
-                    _id: data['classes'][0]['student'][i]['_id'],
-                    name: data['classes'][0]['student'][i]['name'],
-                    lname: data['classes'][0]['student'][i]['lname'],
-                })
+        //console.log(localStorage.getItem('rID'))
+        $.ajax({
+            url: 'random/getClass/' + localStorage.getItem('rID'),
+            contentType: 'application/json',
+            type: 'POST',
+            dataType: 'json',
+            success: function (data) {
+              //  console.log(data)
+                for (let i = 0; i < data['classes'][0]['student'].length; i++) {
+                    stud.push({
+                        _id: data['classes'][0]['student'][i]['_id'],
+                        name: data['classes'][0]['student'][i]['name'],
+                        lname: data['classes'][0]['student'][i]['lname'],
+                    })
+                }
+                console.log(stud.length)
+                let randomStud = stud[Math.floor(Math.random() * stud.length)]
+                console.log(randomStud.name + " " + randomStud.lname)
             }
-           // localStorage.setItem('studObj', JSON.stringify(stud))
-            console.log(JSON.stringify(stud))
-        }
-       })
-
-        
-
+        })
 
 })
 

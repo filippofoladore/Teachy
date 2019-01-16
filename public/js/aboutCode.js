883c36a08a7ddd60d4ded70ef5eb0c80cd5ddba4
Filippo Foladore <filippo.foladore@gmail.com>
2019-01-16 19:24:45 +0100

    About v1.5

--- public/js/aboutCode.js
+++ public/js/aboutCode.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 let accordions = document.getElementsByClassName('accordion');
 for (let i = 0; i < accordions.length; i++) {
     accordions[i].onclick = function(){
@@ -14,8 +15,71 @@ for (let i = 0; i < accordions.length; i++) {
     }
 }
 
-    $('#delAccount').on('click', function(){
-        
-        console.log("hi")
+    $('#del404').on('click', function(event){
+        event.preventDefault();
+        console.log('click')
+        $.confirm({
+            boxWidth: '400px', 
+            icon: 'fa fa-trash-alt',
+            useBootstrap: false,
+            animation: 'top',
+            title: "Eliminazione account",
+            closeAnimation: 'bottom',
+            theme: 'material',
+            type: 'dark',
+            icon: 'fa fa-trash',
+            content: 'Non puoi cancellare un account che non esiste! Fai il login prima o iscriviti!',
+            backgroundDismiss: true, 
+            buttons: {
+                Login: function(){
+                    let url = 'http://localhost:3000/users/login'
+                    $(location).attr('href',url);
+                },
+                Registrati: function(){                    
+                    let url = 'http://localhost:3000/users/register'
+                    $(location).attr('href',url);
+                },
+                Annulla: function(){},
+            }
+        })
     })
+
+    $('#delAccount').on('click', function(event){
+        event.preventDefault();
+        let uid = $('#aboutid').text()
+        uid = uid.replace(/\s/g, '')
+        localStorage.setItem('user', uid);
+        console.log(localStorage.getItem('user'))
+        $.confirm({
+            boxWidth: '400px', 
+            icon: 'fa fa-trash-alt',
+            useBootstrap: false,
+            animation: 'top',
+            title: "Eliminazione account",
+            closeAnimation: 'bottom',
+            theme: 'material',
+            type: 'dark',
+            icon: 'fa fa-trash',
+            content: 'Sei sicuro di voler eliminare definitivamente il tuo account?',
+            autoClose: 'Annulla|8000',
+            backgroundDismiss: true, 
+            buttons: {
+                Procedi: function () {
+                    
+                    $.ajax({
+                        url: '/about/deleteAccount/' + localStorage.getItem('user'),
+                        contentType: 'application/json',
+                        type: 'POST',
+                        success: function (data) {
+                           
+                        }
+                    })
+                },
+                Annulla: function () {
+                    text: 'Annulla'   
+                },
+            }
+        })
+    })
+    
 })
--- routes/about.js
+++ routes/about.js
@@ -7,4 +7,9 @@ router.get('/', function (req, res){
     res.render('about')
 })
 
+router.post('/deleteAccount/:id', function(req, res){
+    res.send("HELLO")
+    console.log(req.user.id)
+})
+
 module.exports = router;
